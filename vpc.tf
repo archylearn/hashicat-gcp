c5941320c423c6f5141a756cc30c80e44cafbcf6
fred10 <arch.def.learn@gmail.com>
2021-05-20 17:33:31 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,11 @@ module "network" {
   source  = "app.terraform.io/fred10-training/network/google"
   version = "2.5.0"
   # insert required variables here
-  network_name = "gaurav-network"
+  network_name = "gaurav10-network"
   project_id = var.project
   subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "gaurav10-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
